8d330b37dcdf3e651aba980b7e3e67d2ed17b029
sebt-max <sebternest@hotmail.com>
2025-01-29 13:10:15 +0100

    Change hello world

--- src/main/java/com/example/jdbci/Main.java
+++ src/main/java/com/example/jdbci/Main.java
@@ -6,7 +6,7 @@ import com.example.jdbci.models.User;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hell World!");
         ConnectionFactory.createDropTable();
         doCrudOnUser();
         doCrudOnAddress();
